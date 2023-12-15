39b7373a308da02e54c1acf94d3444242be1cef7
eljohnevnglsta <112616636+eljohnevnglsta@users.noreply.github.com>
2023-12-16 01:16:15 +0800

    Update on Text Displays.

--- Project/src/application/Walls.java
+++ Project/src/application/Walls.java
@@ -1,6 +1,7 @@
 package application;
 
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import javafx.scene.control.TextField;
@@ -10,10 +11,11 @@ public class Walls extends Sprite{
 	int value;
 //	private Text text;
 	private final static double WALLS_SPEED = GameTimer.BG_SPEED;
-	private final static Image WALLS_IMAGE_RED = new Image("file:src//images/1.png", 125, 125, false, false);
-	private final static Image WALLS_IMAGE_VIOLET = new Image("file:src//images/2.png", 125, 125, false, false);
-	private final static Image WALLS_IMAGE_YELLOW = new Image("file:src//images/3.png", 125, 125, false, false);
-	private final static Image WALLS_IMAGE_BLUE = new Image("file:src//images/4.png", 125, 125, false, false);
+	private final static int IMAGE_SIZE = 125;
+	private final static Image WALLS_IMAGE_RED = new Image("file:src//images/1.png", Walls.IMAGE_SIZE, Walls.IMAGE_SIZE, false, false);
+	private final static Image WALLS_IMAGE_VIOLET = new Image("file:src//images/2.png", Walls.IMAGE_SIZE, Walls.IMAGE_SIZE, false, false);
+	private final static Image WALLS_IMAGE_YELLOW = new Image("file:src//images/3.png", Walls.IMAGE_SIZE, Walls.IMAGE_SIZE, false, false);
+	private final static Image WALLS_IMAGE_BLUE = new Image("file:src//images/4.png", Walls.IMAGE_SIZE, Walls.IMAGE_SIZE, false, false);
 	
 	
 	public Walls(int type, int xPos, int yPos, int charStrength) {
@@ -25,41 +27,20 @@ public class Walls extends Sprite{
 		case 2: this.loadImage(Walls.WALLS_IMAGE_YELLOW); break;
 		default: this.loadImage(Walls.WALLS_IMAGE_BLUE);
 		}
-		TextField t = new TextField();
-		t.setText(Integer.toString(this.value));
-
 	}
 	
 	void move() {
 		this.y += Walls.WALLS_SPEED;
-		if(this.y >= GameStage.WINDOW_HEIGHT){	// if this monster passes through the bottom of the scene, set visible to false
+		if (this.y >= GameStage.WINDOW_HEIGHT){
 			this.vanish();
 		}
 	}
-
-//	public int getValue() {
-//		return value;
-//	}
-//
-//	public void setValue(int value) {
-//		this.value = value;
-//	}
-//	
-//	private void loadWall() {
-//		this.loadImage(Walls.WALLS_IMAGE_RED);
-//		this.loadImage(Walls.WALLS_IMAGE_VIOLET);
-//		this.loadImage(Walls.WALLS_IMAGE_YELLOW);
-//		this.loadImage(Walls.WALLS_IMAGE_BLUE);
-//	}
 	
 	public static int getRandom(int strength) {
 		  Random random = new Random();
 		  int min = Math.max(1, strength/2);
 		  int max = strength + 5;
-		  
 		  int temp = random.nextInt(max - min + 1) + min;
-		  
 		  return (temp);
-		}
-
+	}	
 }
